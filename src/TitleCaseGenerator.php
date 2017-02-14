0372dc42dacd8ca025fefdd814ac9bb7fbb2d0a7
Michaela Davis <michaela@>
2017-02-14 09:33:12 -0800

    pass second test

--- src/TitleCaseGenerator.php
+++ src/TitleCaseGenerator.php
@@ -4,7 +4,12 @@
     {
         function makeTitleCase($input_title)
         {
-            return ucfirst($input_title);
+            $input_array_of_words = explode(" ", $input_title);
+            $output_titledcased = array();
+            foreach ($input_array_of_words as $word) {
+                array_push($output_titledcased, ucfirst($word));
+            }
+            return implode(" ", $output_titledcased);
         }
     }
 
